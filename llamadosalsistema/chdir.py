f493bea6a02899b096af856d1ccdd981b491834a
maherreramu <maherreramu@unal.edu.co>
2021-10-23 18:03:19 -0400

    main

---
+++ llamadosalsistema/chdir.py
@@ -0,0 +1,15 @@
+import os, sys
+
+#El metodo "os.chdir(path)" solicita como argumento un str con la ruta
+#a la cual se cambiará el espacio de tranajo
+
+actdir = os.getcwd()
+print("directorio actual = ", actdir)
+path = input("Ingrese la ruta a la cual desea cambiar el directorio de trabajo: ")
+
+try:
+    os.chdir(path)
+    actdir = os.getcwd()
+    print("directorio de trabajo actualizado a: ", actdir)
+except Exception as e:
+    print("Error: ", os.strerror(e.errno))
---
+++ llamadosalsistema/getcwd.py
@@ -0,0 +1,7 @@
+import os
+
+#El metodo "os.getcwd()" retorna un string con la ruta al
+#directorio de trabajo actual
+
+path = os.getcwd()
+print(path)
