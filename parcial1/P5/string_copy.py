b870932b432ae9e4ed4fd41045e1268089eeacf6
maherreramu <maherreramu@unal.edu.co>
2021-11-19 00:45:53 -0500

    Update string_copy.py

--- parcial1/P5/string_copy.py
+++ parcial1/P5/string_copy.py
@@ -1,22 +1,22 @@
 
-#Fue necesario buscar la variable destino dentro el listado ce variables globales
+#Fue necesario buscar la variable destino dentro el listado de variables globales
 #ya que al reasignar la variable destino de forma directa se realiza localmente.
 #Es decir, unicamente dentro de la función.
 #Para lograr encontrar la variable destino dentro de el listado de variables globales
 #fue necesario hacer una comparación por el id de la variable destino
 def str_copy(str1, str2):
-    listOfGlobals = globals()
+    lglobals = globals()
     s2 = ''
-    for name in listOfGlobals:
-        if id(name) == id(str2):
-            s2 = name
+    for var in lglobals:
+        if id(var) == id(str2):
+            s2 = var
             break
     if s2 == '':
         print('Variable destino no creada globalmente')
         return
-    listOfGlobals[s2] = ''
+    lglobals[s2] = ''
     for i in range(len(str1)):
-        listOfGlobals[s2] += str1[i]
+        lglobals[s2] += str1[i]
     return 
 
 str1 = 'Hola mundo'
